cffc7669b3af67ca089d32b48dca5175ee262be7
Leandro Parazito <leandroh@gmail.com>
2015-12-14 16:26:51 -0200

    Resolves info.json file into an absolute path

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
-var filename = './info.json';
+var path = require('path');
+var filename = path.resolve(__dirname, 'info.json');
 
 fs.readFile(filename, 'utf8', function(err, data) {
   console.log(err || data);
